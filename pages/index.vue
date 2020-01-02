03ae5044fe006eea755ce6f1af30d35c9c17b496
Tomonari Takai <takai01010100@gmail.com>
2020-01-02 17:32:19 +0900

    Fix Page:Index

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,15 @@
 <template lang="pug">
   .pageIndex
-    Indicator.base
-    DigitalClock
+    VueToNuxtLogo
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-
-// import AuthorInfo from '~/components/Molecule/AuthorInfo/index.vue'
-import DigitalClock from '~/components/Organism/DigitalClock/index.vue'
-import Indicator from '~/components/Organism/Indicator/index.vue'
+import VueToNuxtLogo from '@/components/Atom/VueToNuxtLogo/index.vue'
 
 export default {
   name: 'PageIndex',
   components: {
-    DigitalClock,
-    Indicator,
-  },
-  data() {
-    return {
-      timer: null,
-    }
-  },
-  created() {
-    this.getDate()
-  },
-  mounted() {
-    this.timer = setInterval(this.getDate, 1000)
-  },
-  destroyed() {
-    clearInterval(this.timer)
-  },
-  methods: {
-    ...mapActions(['getDate']),
+    VueToNuxtLogo,
   },
 }
 </script>
@@ -48,10 +25,4 @@ export default {
   align-content: center
   align-items: center
   overflow: hidden
-
-  .base
-    position: absolute
-    top: 0
-    left: 0
-    z-index: -1
 </style>
