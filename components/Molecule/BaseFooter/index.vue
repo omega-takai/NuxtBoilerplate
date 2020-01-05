b01069dc2d5abe0f99f284e898b5b8d4ed1a802f
Tomonari Takai <takai01010100@gmail.com>
2020-01-05 20:17:22 +0900

    create footer

---
+++ components/Molecule/BaseFooter/index.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+  footer(:class="$style.footer")
+    dl
+      div
+        dt Vuex Binding
+        dd {{ bindStatus }}
+      div
+        dt GitHub
+        dd discription
+      div
+        dt Author
+        dd
+          div {{ authorName }} ({{ authorPosition }})
+          div Twitter
+
+    p © 2020 {{ authorName }}.
+</template>
+
+<script>
+export default {
+  name: 'BaseFooter',
+  props: {
+    bindStatus: {
+      type: String,
+      default: 'Vuex Binding',
+    },
+    repoUrl: {
+      type: String,
+      default: 'https://github.com/nuxt/nuxt.js',
+    },
+    authorName: {
+      type: String,
+      default: 'Author Name',
+    },
+    authorPosition: {
+      type: String,
+      default: 'UI Designer / Front-end Engineer',
+    },
+    twitterUrl: {
+      type: String,
+      default: 'https://twitter.com/nuxt_js',
+    },
+  },
+}
+</script>
+
+<style lang="sass" module>
+.footer
+  background-color: colors('inverted')
+  color: colors('white')
+  font-size: TypeScale('body')
+  font-weight: TypeWeight('body')
+  padding: 3em 5em 1em
+</style>
