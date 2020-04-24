a52faa9e4773d4eb3646f9a448ea6d4c7c3f9fc1
Tomonari Takai <takai01010100@gmail.com>
2020-04-24 14:05:12 +0900

    [Change Directory Structure] storybook

--- src/.storybook/config.js
+++
@@ -1,6 +0,0 @@
-import { configure } from '@storybook/vue';
-
-import '!style-loader!css-loader?sourceMap!sass-loader!../assets/style/global.scss';
-
-// automatically import all files ending in *.story.js
-configure(require.context('../components', true, /\.story\.js$/), module);
