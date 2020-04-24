a35b6fbde6ac5f242e9a9e03f2ee601de9f895a8
Tomonari Takai <takai01010100@gmail.com>
2020-04-24 16:14:23 +0900

    [BaseComponentNames] nuxt logo

--- src/components/Atom/TheNuxtLogo/index.vue
+++ src/components/Atom/TheNuxtLogo/index.vue
@@ -1,7 +1,7 @@
 <script src="./index.js"></script>
 <style lang="scss" src="./index.scss" module></style>
 <template lang="pug">
-  div(:class="$style.vueToNuxtLogo")
+  div(:class="$style.logo")
     div(:class="$style.two")
     div(:class="$style.one")
     div(:class="$style.three")
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import BaseLayout from '@/components/Template/BaseLayout/index.vue'
-import VueToNuxtLogo from '@/components/Atom/VueToNuxtLogo/index.vue'
+import TheNuxtLogo from '~/components/Atom/TheNuxtLogo/index.vue'
 import Typography from '@/components/Atom/Typography/index.vue'
 
 export default {
   name: 'PageIndex',
   components: {
     BaseLayout,
-    VueToNuxtLogo,
+    TheNuxtLogo,
     Typography,
   },
 }
