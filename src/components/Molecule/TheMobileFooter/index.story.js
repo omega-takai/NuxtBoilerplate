a15e12b939ae028d8d18852d632e65d37add808f
Tomonari Takai <takai01010100@gmail.com>
2020-04-24 17:31:58 +0900

    Create TheMobileFooter

---
+++ src/components/Molecule/TheMobileFooter/index.story.js
@@ -0,0 +1,47 @@
+import { storiesOf } from '@storybook/vue'
+import { withKnobs, text } from '@storybook/addon-knobs'
+import TheMobileFooter from './index.vue'
+import { Wrapper } from '@/assets/styledComponent/component'
+
+const stories = storiesOf('Design System|Molecule/TheMobileFooter', module)
+
+stories.addDecorator(withKnobs)
+
+stories.add('normal', () => ({
+  components: {
+    TheMobileFooter,
+    Wrapper,
+  },
+  props: {
+    bindStatus: {
+      type: String,
+      default: text('bindStatus', 'Vuex Binding'),
+    },
+    repoUrl: {
+      type: String,
+      default: text('repoUrl', 'https://github.com/nuxt/nuxt.js'),
+    },
+    authorName: {
+      type: String,
+      default: text('authorName', 'Author Name'),
+    },
+    authorPosition: {
+      type: String,
+      default: text('authorPosition', 'UI Designer / Front-end Engineer'),
+    },
+    twitterUrl: {
+      type: String,
+      default: text('twitterUrl', 'https://twitter.com/nuxt_js'),
+    },
+  },
+  template: `
+    <Wrapper>
+      <TheMobileFooter
+        :bindStatus="bindStatus"
+        :repoUrl="repoUrl"
+        :authorName="authorName"
+        :authorPosition="authorPosition"
+        :twitterUrl="twitterUrl"
+      />
+    </Wrapper>`,
+}))
