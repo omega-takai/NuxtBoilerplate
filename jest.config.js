422a883f9d3d92304b265fc70b2883e681489a79
Tomonari Takai <takai01010100@gmail.com>
2020-05-14 22:58:52 +0900

    revert jest settings

--- jest.config.js
+++ jest.config.js
@@ -8,17 +8,17 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest',
-    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
-      'jest-transform-stub',
+    // '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+    //   'jest-transform-stub',
   },
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/components/**/*.vue'],
-  globals: {
-    'vue-jest': {
-      resources: {
-        scss: ['./src/assets/style/_not-actual-styles.scss'],
-      },
-      experimentalCSSCompile: true,
-    },
-  },
+  // globals: {
+  //   'vue-jest': {
+  //     resources: {
+  //       scss: ['./src/assets/style/_not-actual-styles.scss'],
+  //     },
+  //     experimentalCSSCompile: true,
+  //   },
+  // },
 }
