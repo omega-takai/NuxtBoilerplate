a902040f5936ffc0ba731923579eb69b6d7000cf
Tomonari Takai <takai01010100@gmail.com>
2020-08-23 16:08:47 +0900

    Revert "Revert "Templateのインデントレベルを下げる""
    This reverts commit 60c1b366f07fe64d0bcbd747505595da3d0040bc.

--- src/components/Atom/BaseLinkButton/index.vue
+++ src/components/Atom/BaseLinkButton/index.vue
@@ -1,12 +1,12 @@
 <script src="./index.js"></script>
 <style lang="scss" src="./index.scss" scoped></style>
 <template lang="pug">
-  a(
-    target="_blank"
-    :title="text"
-    :href="url"
-    :class="className"
-  )
-    slot
-    span(v-text="text")
+a(
+  target="_blank"
+  :title="text"
+  :href="url"
+  :class="className"
+)
+  slot
+  span(v-text="text")
 </template>
--- src/components/Molecule/LinkTextWithIcon/index.vue
+++ src/components/Molecule/LinkTextWithIcon/index.vue
@@ -1,14 +1,14 @@
 <script src="./index.js"></script>
 <style lang="scss" src="./index.scss" scoped></style>
 <template lang="pug">
-  BaseLinkText(
-    :url="url"
-    :colorType="colorType"
-    :text="text"
+BaseLinkText(
+  :url="url"
+  :colorType="colorType"
+  :text="text"
+)
+  BaseFeatherIcon(
+    :iconName="iconName"
+    :iconSize="iconSize"
+    class="icon"
   )
-    BaseFeatherIcon(
-      :iconName="iconName"
-      :iconSize="iconSize"
-      class="icon"
-    )
 </template>
