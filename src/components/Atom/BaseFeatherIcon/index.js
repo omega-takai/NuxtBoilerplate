9a4f0aba6785d44ebd1660552f60c0682072fce8
Tomonari Takai <takai01010100@gmail.com>
2020-10-24 22:12:08 +0900

    アイコンの種類追加

--- src/components/Atom/BaseFeatherIcon/index.js
+++ src/components/Atom/BaseFeatherIcon/index.js
@@ -14,6 +14,7 @@ import {
   GithubIcon,
   InstagramIcon,
   TwitterIcon,
+  FileTextIcon,
 } from 'vue-feather-icons'
 
 export default {
@@ -31,6 +32,7 @@ export default {
     GithubIcon,
     InstagramIcon,
     TwitterIcon,
+    FileTextIcon,
   },
   props: {
     iconSize: {
@@ -57,6 +59,7 @@ export default {
           'GithubIcon',
           'InstagramIcon',
           'TwitterIcon',
+          'FileTextIcon',
         ].includes(value)
       },
     },
