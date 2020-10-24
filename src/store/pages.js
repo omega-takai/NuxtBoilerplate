33a93155c156d15e8d3dc9031e8a6f623e6832e6
Tomonari Takai <takai01010100@gmail.com>
2020-10-24 18:17:48 +0900

    参考ドキュメントのリストを作成

--- src/store/pages.js
+++ src/store/pages.js
@@ -9,6 +9,36 @@ export const state = () => ({
       path: '/OtherPage',
     },
   ],
+  reference: [
+    {
+      name: 'NuxtJS',
+      path: 'https://ja.nuxtjs.org/',
+    },
+    {
+      name: 'Vue.js',
+      path: 'https://jp.vuejs.org/',
+    },
+    {
+      name: 'スタイルガイド — Vue.js',
+      path: 'https://jp.vuejs.org/v2/style-guide/index.html',
+    },
+    {
+      name: 'Vuex とは何か？ | Vuex',
+      path: 'https://vuex.vuejs.org/ja/',
+    },
+    {
+      name: 'Nuxt Storybook',
+      path: 'https://storybook.nuxtjs.org/',
+    },
+    {
+      name: 'Sass: Syntactically Awesome Style Sheets',
+      path: 'https://sass-lang.com/',
+    },
+    {
+      name: 'Pug - A high-performance template engine',
+      path: 'https://pugjs.org/',
+    },
+  ],
 })
 
 export const getters = {}
