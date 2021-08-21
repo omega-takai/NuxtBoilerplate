33192619a32ede6b62da6800a76950dcf62f0242
Tomonari Takai <takai01010100@gmail.com>
2021-08-21 22:33:44 +0900

    fix: delete old components

--- src/components/Atom/BaseTypography/index.vue
+++
@@ -1,9 +0,0 @@
-<script src="./index.js"></script>
-<template lang="pug">
-  component(
-    :is="tagName"
-    :class="$style[tagName]"
-    v-text="text"
-  )
-</template>
-<style lang="scss" src="./index.scss" module></style>
