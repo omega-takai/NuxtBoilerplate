895e0453d0676e56ec3440666003e999ab022c5d
Tomonari Takai <takai01010100@gmail.com>
2021-08-22 03:39:20 +0900

    feat: add storybook

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,14 +3,10 @@ module.exports = {
     'stylelint-config-recommended',
     'stylelint-config-standard',
     'stylelint-config-sass-guidelines',
-    'stylelint-config-rational-order',
     // 'stylelint-config-primer',
     'stylelint-config-prettier'
   ],
   plugins: [
-    'stylelint-scss',
-    'stylelint-order',
-    'stylelint-config-rational-order/plugin'
     // "stylelint-config-primer/plugin"
   ],
   // add your custom config here
