ec1581612cabdd9c0a612609641a6d95bd69a507
Tomonari Takai <takai01010100@gmail.com>
2021-08-23 15:09:07 +0900

    fix: update lint
    https://prettier.io/docs/en/options.html#semicolons
    https://prettier.io/docs/en/options.html#trailing-commas
    https://prettier.io/docs/en/options.html#quotes
    https://stylelint.io/user-guide/usage/cli
    https://eslint.org/docs/user-guide/configuring/ignoring-code

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,17 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
-    'prettier'
+    'prettier',
   ],
+  ignorePatterns: ['!.storybook/*.js'],
   // add your custom rules here
   rules: {
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'error'
-  }
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
 }
