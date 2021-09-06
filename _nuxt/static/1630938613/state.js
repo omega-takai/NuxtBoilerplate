6283a91690f84870db354b6c4d681325f03e1870
t--takai <t--takai@users.noreply.github.com>
2021-09-06 14:30:14 +0000

    deploy: 3229c8f0f3554dd49f0ab1939e3975cf302e4144

---
+++ _nuxt/static/1630938613/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FNuxtBoilerplate\u002F_nuxt\u002Fstatic\u002F1630938613",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FNuxtBoilerplate\u002F",assetsPath:"\u002FNuxtBoilerplate\u002F_nuxt\u002F",cdnURL:a}}}}(null));
