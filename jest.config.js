726c57547b0d40a8b8dfbae885453720aabe281e
Tomonari Takai <takai01010100@gmail.com>
2021-09-13 00:50:42 +0900

    fix: jest sass error

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^~/(.*)$': '<rootDir>/src/$1',
+    '^@@/(.*)$': '<rootDir>/$1',
+    '^~~/(.*)$': '<rootDir>/$1',
     '^vue$': 'vue/dist/vue.common.js',
   },
   moduleFileExtensions: ['js', 'vue', 'json'],
