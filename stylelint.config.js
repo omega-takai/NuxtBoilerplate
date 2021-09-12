f4b742d5e18334f53e7a3b6146013d73de1896c8
Tomonari Takai <takai01010100@gmail.com>
2021-09-12 22:40:21 +0900

    fix: lint-extends
    https://github.com/stylelint/awesome-stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-config-recommended',
+    'stylelint-config-recommended-scss',
+    'stylelint-config-prettier',
+  ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {},
